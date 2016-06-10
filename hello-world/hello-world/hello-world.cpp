cd16d4bff5bc8feb9b8ee7f6b7769f02c9037555
usingNamespace2015 <josh_mortenson@hotmail.com>
2016-06-10 10:01:45 -0600

    added hello-world project.

---
+++ hello-world/hello-world/hello-world.cpp
@@ -0,0 +1,16 @@
+// hello-world.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+	string mystr = "Hello World";
+	cout << mystr << endl;
+	cin.ignore();
+	return 0;
+}
+
